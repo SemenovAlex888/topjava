0342606b31c3a897ef0e39fa42ec8c6e6a555b95
SemenovAlex888 <aurum25@ya.ru>
2021-02-26 17:19:41 +0300

    HW 3.6 update Meal

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
@@ -44,7 +44,9 @@ public class JdbcMealRepository implements MealRepository {
         if(meal.isNew()) {
             Number newKey = insertMeal.executeAndReturnKey(map);
             meal.setId(newKey.intValue());
-        } else if (namedParameterJdbcTemplate.update("UPDATE meals SET user_id=:userId, date_time=:dateTime, description=:description, calories=:calories", map) == 0) {
+        } else if (namedParameterJdbcTemplate.update("UPDATE meals " +
+                "SET date_time=:dateTime, description=:description, calories=:calories" +
+                " WHERE id=:id AND user_id=:userId", map) == 0) {
             return null;
         }
         return meal;
@@ -68,6 +70,7 @@ public class JdbcMealRepository implements MealRepository {
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return jdbcTemplate.query("SELECT * FROM meals WHERE user_id=? AND date_time BETWEEN ? AND ? ORDER BY date_time DESC", ROW_MAPPER, userId, startDateTime, endDateTime);
+        return jdbcTemplate.query("SELECT * FROM meals WHERE user_id=? AND date_time >= ? AND date_time < ? ORDER BY date_time DESC",
+                ROW_MAPPER, userId, startDateTime, endDateTime);
     }
 }
