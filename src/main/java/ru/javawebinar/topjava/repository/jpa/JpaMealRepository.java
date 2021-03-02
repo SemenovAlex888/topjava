4d87b8fdfb2d44dfd1adfeddac1536ea0af4df6a
SemenovAlex888 <aurum25@ya.ru>
2021-03-02 13:18:35 +0300

    HW 4.1 methods implementation

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -1,37 +1,62 @@
 package ru.javawebinar.topjava.repository.jpa;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public class JpaMealRepository implements MealRepository {
 
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
+    @Transactional
     public Meal save(Meal meal, int userId) {
-        return null;
+        if(meal.isNew()) {
+            em.persist(meal);
+            return meal;
+        } else {
+            return em.merge(meal);
+        }
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId) {
-        return false;
+        return em.createNamedQuery(Meal.DELETE).
+                setParameter("id", id).
+                executeUpdate() != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return em.find(Meal.class, id);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        List resultList = em.createNamedQuery(Meal.ALL_SORTED).
+                setParameter(1, User.class).
+                getResultList();
+        return resultList;
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        List resultList = em.createNamedQuery(Meal.BY_DATE).
+                setParameter(1, User.class).
+                setParameter(2, startDateTime).
+                setParameter(3, endDateTime).
+                getResultList();
+        return resultList;
     }
 }
